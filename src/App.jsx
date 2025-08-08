e0f9a3150944d5a910ceb84fd50e4e6dafb7d343
Sandesh7888 <sandeshkarad7472@gmail.com>
2025-08-08 22:23:48 +0530

    new page

--- src/App.jsx
+++ src/App.jsx
@@ -10,36 +10,40 @@ import AuthContext from './context/AuthProvider.jsx'
 function App() {
   const [user, setUser] = useState(null)
 
+  const [loggingUser, setLoggingUser] = useState(null)
+
   const authData = useContext(AuthContext);
 
 
-  useEffect(() => {
-    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
-    if (loggedInUser) {
-      setUser(loggedInUser.role);
-    }
-  }, [authData]);
+  // useEffect(() => {
+  //   const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+  //   if (loggedInUser) {
+  //     setUser(loggedInUser.role);
+  //   }
+  // }, [authData]);
 
 
   const handleLogin = (email,password) => {
     if(email=='admin@me.com' && password==123){
       setUser('admin');
       setLocalStorage('loggedInUser', JSON.stringify({ role: 'admin' }));
-    }else if(authData && authData.userData && authData.userData.employees.find(emp => emp.email === email && emp.password === password)){
-      setUser('employee');
-       setLocalStorage('loggedInUser', JSON.stringify({ role: 'employee' }));
-    }else{
-      setUser('Invalid credentials');
+    }else if(authData ){
+      const employee = authData.find(user => user.email === email && user.password === password);
+      if(employee){
+        setUser('employee');
+        setLocalStorage('loggedInUser', JSON.stringify({ role: 'employee' }));
+      }else{
+        setUser('Invalid credentials');
+      }
     }
   }
-
   
   
 
   return (
     <>
       { !user ? <Login handleLogin={handleLogin} /> : ''}
-      { user === 'admin' ? <AdminDashboard /> : <EmployeeDashboard /> }
+      { user === 'admin' ? <AdminDashboard /> : <EmployeeDashboard data={employee} /> }
     </>
   )
 }
--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,12 +3,12 @@ import Header from '../UI/Header'
 import TaskListNumber from '../UI/TaskListNumber'
 import TaskList from '../TaskList/TaskList'
 
-const EmployeeDashboard = () => {
+const EmployeeDashboard = ({ props }) => {
   return (
     <div className='p-4'>
         <Header />
         <TaskListNumber />
-        <TaskList />
+        <TaskList data={props.data} />
         
       
     </div>
